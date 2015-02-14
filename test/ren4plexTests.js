ea71fe4e69cb738a137c7c6204aa4c5df3932d88
ZaX <zacchi.paolo@gmail.com>
2015-02-14 12:51:45 +0100

    Aggiunta libreria commander per gestire i parametri da linea di comando,
    -v --version,
    -p --preview,
    -h --help

--- test/ren4plexTests.js
+++ test/ren4plexTests.js
@@ -57,6 +57,8 @@ describe('ren4plex', function(){
                     out: 'Gotham s01e11 Rogues Gallery.mkv'}
                 ,{  in:  'Marvels.Agents.of.S.H.I.E.L.D.S02E08.720p.WEB-DL.2CH.x265.AlgernonWood.mkv',
                     out: 'Marvels Agents Of S H I E L D s02e08.mkv'}
+                ,{  in:  'Once.Upon.A.Time.4X07.La.Regina.Delle.Nevi.ITA.ENG.720p.WEB-DLMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
+                    out: 'Once Upon A Time s04e07 La Regina Delle Nevi.mkv'}
             ];
             for (var i = 0; i < testCases.length; i++) {
                 assert.equal(ren4plex.getFilename(testCases[i].in), testCases[i].out);
