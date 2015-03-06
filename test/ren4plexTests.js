8378165583aa1a6fc346f63b81a727427e844ed2
ZaX <zacchi.paolo@gmail.com>
2015-03-06 21:49:49 +0100

    add it.each for tests

--- test/ren4plexTests.js
+++ test/ren4plexTests.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var ren4plex = require('../lib/ren4plex');
+require('it-each')({ testPerIteration: true });
 
 describe('ren4plex', function(){
 
@@ -40,57 +41,41 @@ describe('ren4plex', function(){
 
     describe('#getFilename()', function(){
 
-        it('should parse name correctly', function(){
+        var testCases = [
+            {   in:  'A.Proposito.di.Davis.(2013).1080p.BluRay.Dts.ITA.ENG.Subs.x264.mkv',
+                out: 'A Proposito Di Davis (2013).mkv'}
+            ,{  in:  'Lucy.2014.iTA-ENG.Bluray.720p.x264-TRL.mkv',
+                out: 'Lucy (2014).mkv'}
+            ,{  in:  'Boxtrolls-Le.Scatole.Magiche.2014.DTS.ITA.ENG.1080p.BluRay.x264-BLUWORLD.mkv',
+                out: 'Boxtrolls Le Scatole Magiche (2014).mkv'}
+            ,{  in:  'the.originals.101.hdtv-lol.mp4',
+                out: 'The Originals s01e01.mp4'}
+            ,{  in:  'Continuum.3X06.Una.Decisione.Difficile.ITA.ENG.720p.BDMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
+                out: 'Continuum s03e06 Una Decisione Difficile.mkv'}
+            ,{  in:  '[DLMux 720p - H264 - Ita Mp3] Elementary S01e01.by.IperB.mkv',
+                out: 'Elementary s01e01.mkv'}
+            ,{  in:  'Jane.The.Virgin.S01E01.PROPER.HDTV.x264-2HD.mp4',
+                out: 'Jane The Virgin s01e01.mp4'}
+            ,{  in:  'Gotham.S01E11.Rogues.Gallery.720p.WEB-DL.2CH.x265.HEVC-AlgernonWood.mkv',
+                out: 'Gotham s01e11 Rogues Gallery.mkv'}
+            ,{  in:  'Marvels.Agents.of.S.H.I.E.L.D.S02E08.720p.WEB-DL.2CH.x265.AlgernonWood.mkv',
+                out: 'Marvels Agents Of Shield s02e08.mkv'}
+            ,{  in:  'Once.Upon.A.Time.4X07.La.Regina.Delle.Nevi.ITA.ENG.720p.WEB-DLMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
+                out: 'Once Upon A Time s04e07 La Regina Delle Nevi.mkv'}
+            ,{  in:  'Person.of.Interest.4X03.L.Esperto.Di.Donne.ITA.ENG.720p.Web-DMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
+                out: 'Person Of Interest s04e03 L Esperto Di Donne.mkv'}
+            ,{  in:  'Hart.of.Dixie.S04E06.HDTV.x264-LOL.mp4',
+                out: 'Hart Of Dixie s04e06.mp4'}
+            ,{  in:  '[Mux - 720p - H264 - Ita Eng Mp3 - sub Ita Eng] The Blacklist S02e03 - Dr James Covington.by.IperB.mkv',
+                out: 'The Blacklist s02e03 Dr James Covington.mkv'}
+            ,{  in:  'Marvels.Agents.of.S.H.I.E.L.D.S02E11.HDTV.x264-KILLERS.[VTV].mp4',
+                out: 'Marvels Agents Of Shield s02e11.mp4'}
+            ,{  in:  'Revenge.4X03.Ceneri.ITA.ENG.720p.WEB-DLMux.H.265-iGM.GiuseppeTnT-Marco_kh.mkv',
+                out: 'Revenge s04e03 Ceneri.mkv'}
+        ];
 
-            var testCases = [
-                {   in:  'A.Proposito.di.Davis.(2013).1080p.BluRay.Dts.ITA.ENG.Subs.x264.mkv',
-                    out: 'A Proposito Di Davis (2013).mkv'}
-                ,{  in:  'Lucy.2014.iTA-ENG.Bluray.720p.x264-TRL.mkv',
-                    out: 'Lucy (2014).mkv'}
-                ,{  in:  'Boxtrolls-Le.Scatole.Magiche.2014.DTS.ITA.ENG.1080p.BluRay.x264-BLUWORLD.mkv',
-                    out: 'Boxtrolls Le Scatole Magiche (2014).mkv'}
-                ,{  in:  'the.originals.101.hdtv-lol.mp4',
-                    out: 'The Originals s01e01.mp4'}
-                ,{  in:  'Continuum.3X06.Una.Decisione.Difficile.ITA.ENG.720p.BDMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
-                    out: 'Continuum s03e06 Una Decisione Difficile.mkv'}
-                ,{  in:  '[DLMux 720p - H264 - Ita Mp3] Elementary S01e01.by.IperB.mkv',
-                    out: 'Elementary s01e01.mkv'}
-                ,{  in:  'Jane.The.Virgin.S01E01.PROPER.HDTV.x264-2HD.mp4',
-                    out: 'Jane The Virgin s01e01.mp4'}
-                ,{  in:  'Gotham.S01E11.Rogues.Gallery.720p.WEB-DL.2CH.x265.HEVC-AlgernonWood.mkv',
-                    out: 'Gotham s01e11 Rogues Gallery.mkv'}
-                ,{  in:  'Marvels.Agents.of.S.H.I.E.L.D.S02E08.720p.WEB-DL.2CH.x265.AlgernonWood.mkv',
-                    out: 'Marvels Agents Of Shield s02e08.mkv'}
-                ,{  in:  'Once.Upon.A.Time.4X07.La.Regina.Delle.Nevi.ITA.ENG.720p.WEB-DLMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
-                    out: 'Once Upon A Time s04e07 La Regina Delle Nevi.mkv'}
-                ,{  in:  'Person.of.Interest.4X03.L.Esperto.Di.Donne.ITA.ENG.720p.Web-DMux.x265.HEVC-iGM+GiuseppeTnT+Marco_kh.mkv',
-                    out: 'Person Of Interest s04e03 L Esperto Di Donne.mkv'}
-                ,{  in:  'Hart.of.Dixie.S04E06.HDTV.x264-LOL.mp4',
-                    out: 'Hart Of Dixie s04e06.mp4'}
-                ,{  in:  '[Mux - 720p - H264 - Ita Eng Mp3 - sub Ita Eng] The Blacklist S02e03 - Dr James Covington.by.IperB.mkv',
-                    out: 'The Blacklist s02e03 Dr James Covington.mkv'}
-                ,{  in:  'Marvels.Agents.of.S.H.I.E.L.D.S02E11.HDTV.x264-KILLERS.[VTV].mp4',
-                    out: 'Marvels Agents Of Shield s02e11.mp4'}
-                ,{  in:  'Revenge.4X03.Ceneri.ITA.ENG.720p.WEB-DLMux.H.265-iGM.GiuseppeTnT-Marco_kh.mkv',
-                    out: 'Revenge s04e03 Ceneri.mkv'}
-            ];
-
-            for (var i = 0; i < testCases.length; i++) {
-                assert.equal(ren4plex.getFilename(testCases[i].in), testCases[i].out);
-            }
-
-        })
-        it('should parse name correctly, debug', function(){
-
-            var testCases = [
-                {  in:  'Revenge.4X03.Ceneri.ITA.ENG.720p.WEB-DLMux.H.265-iGM.GiuseppeTnT-Marco_kh.mkv',
-                    out: 'Revenge s04e03 Ceneri.mkv'}
-            ];
-
-            for (var i = 0; i < testCases.length; i++) {
-                assert.equal(ren4plex.getFilename(testCases[i].in), testCases[i].out);
-            }
-
+        it.each(testCases, 'should rename  "%s" -> "%s"', ['in','out'], function(element){
+            assert.equal(ren4plex.getFilename(element.in), element.out);
         })
 
     })
